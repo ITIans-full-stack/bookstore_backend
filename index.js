4a9b31d156f650c8bc76745cafc37ea2fed80dcb
Noura ElBrashy <noura.elbrashy@gmail.com>
2025-06-29 23:59:27 +0300

    Resolve merge conflicts after applying stash

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ const userRoutes = require('./routes/userRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 // const { notFound, errorHandler } = require('./middleware/errorHandler');
 const errorHandler = require('./middleware/errorHandler');
+
+const { notFound, errorHandler } = require('./middleware/errorHandler');
+const orderRoutes = require('./routes/orders');
+
+
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
 
-
 // Routes
 // app.use('/api/users', userRoutes);
 app.use('/api/books', bookRoutes);
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 // Error Handling
 // app.use(notFound);
 app.use(errorHandler);
