592f06a01e94021275ee7a368ddb9c4424a998b3
AbdallahBahaa <abdallah.bahaa2000@gmail.com>
2025-07-21 00:50:46 +0300

    cors agaain edits

--- server.js
+++ server.js
@@ -132,12 +132,18 @@ connectDB();
 // ====== INIT APP FIRST ======
 const app = express();
 
+app.use(cors({
+  origin: 'https://bookstores-fawn.vercel.app/landing',
+  credentials: true
+}));
+
 //  Very important: webhook must come before express.json()
 app.use(
   "/api/webhook",
   express.raw({ type: "application/json" }),
   stripeWebhook
 );
+
 
 // ====== Middleware ======
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
@@ -216,10 +222,7 @@ app.use(
 
 
 
-app.use(cors({
-  origin: 'https://bookstores-fawn.vercel.app/landing',
-  credentials: true
-}));
+
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
