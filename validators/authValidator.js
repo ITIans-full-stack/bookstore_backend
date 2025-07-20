55c90d527d06acbc406a0470d384612b8145ffb5
Noura ElBrashy <noura.elbrashy@gmail.com>
2025-07-20 19:25:42 +0300

    update order controller and authvalidator

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -1,5 +1,54 @@
 // validators/authValidator.js
+// const Joi = require('joi');
+
+// exports.registerSchema = Joi.object({
+//   name: Joi.string().min(3).required().messages({
+//     'string.empty': 'Name is required',
+//     'string.min': 'Name must be at least 3 characters'
+//   }),
+//   email: Joi.string().email().required().messages({
+//     'string.email': 'Invalid email format',
+//     'string.empty': 'Email is required'
+//   }),
+//   password: Joi.string().min(6).required().messages({
+//     'string.min': 'Password must be at least 6 characters',
+//     'string.empty': 'Password is required'
+//   })
+// });
+
+// exports.loginSchema = Joi.object({
+//   email: Joi.string().email().required().messages({
+//     'string.email': 'Invalid email format',
+//     'string.empty': 'Email is required'
+//   }),
+//   password: Joi.string().required().messages({
+//     'string.empty': 'Password is required'
+//   })
+// });
+
+// exports.updateProfileSchema = Joi.object({
+//   name: Joi.string().min(3),
+//   email: Joi.string().email(),
+//   password: Joi.string().min(6)
+// });
+
+// validators/authValidator.js
 const Joi = require('joi');
+
+// Password must include:
+// - Min 8 characters
+// - At least one uppercase
+// - At least one lowercase
+// - At least one number
+// - At least one special character
+
+const strongPassword = Joi.string()
+  .pattern(new RegExp('^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[!@#$%^&])[A-Za-z\d!@#$%^&]{8,}$'))
+  .required()
+  .messages({
+    'string.pattern.base': 'Password must be at least 8 characters and include uppercase, lowercase, number, and special character',
+    'string.empty': 'Password is required'
+  });
 
 exports.registerSchema = Joi.object({
   name: Joi.string().min(3).required().messages({
@@ -10,10 +59,7 @@ exports.registerSchema = Joi.object({
     'string.email': 'Invalid email format',
     'string.empty': 'Email is required'
   }),
-  password: Joi.string().min(6).required().messages({
-    'string.min': 'Password must be at least 6 characters',
-    'string.empty': 'Password is required'
-  })
+  password: strongPassword
 });
 
 exports.loginSchema = Joi.object({
@@ -29,5 +75,9 @@ exports.loginSchema = Joi.object({
 exports.updateProfileSchema = Joi.object({
   name: Joi.string().min(3),
   email: Joi.string().email(),
-  password: Joi.string().min(6)
+  password: Joi.string()
+    .pattern(new RegExp('^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[!@#$%^&])[A-Za-z\d!@#$%^&]{8,}$'))
+    .messages({
+      'string.pattern.base': 'Password must be at least 8 characters and include uppercase, lowercase, number, and special character'
+    })
 });
